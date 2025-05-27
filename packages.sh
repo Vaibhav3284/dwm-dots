f201d3816ad52aac55608cb01cdb07a0a43a19fc
bOred <125344705+Vaibhav3284@users.noreply.github.com>
2025-05-27 16:32:26 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 echo "installing..."
 
-sudo pacman -S --noconfirm wireplumber pulseaudio pavucontrol kitty nvim dunst picom rofi pulseaudio-bluetooth bluez bluez-utils blueman flameshot feh ranger vlc nwg-look fzf htop ntfs-3g xclip wine zsh ttf-font-awesome ttf-hack-nerd ttf-jetbrains-mono-nerd ttf-roboto-mono-nerd tumbler npm gvfs thunar thunar-archive-plugin polkit-kde-agent xarchiver wine ripgrep zsh
+sudo pacman -S --noconfirm wireplumber pulseaudio pavucontrol kitty nvim dunst picom rofi pulseaudio-bluetooth bluez bluez-utils blueman flameshot feh ranger vlc nwg-look fzf htop ntfs-3g xclip zsh ttf-font-awesome ttf-hack-nerd ttf-jetbrains-mono-nerd ttf-roboto-mono-nerd tumbler npm gvfs thunar thunar-archive-plugin polkit-kde-agent xarchiver wine ripgrep
 yay -S --noconfirm moc-pulse mocp-themes-git sct everforest-gtk-theme-git everforest-icon-theme-git
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
